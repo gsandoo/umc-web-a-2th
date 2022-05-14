b1bf97769935fe5a9cf54c989e488915134f6b4c
gsandoo <rogersddt@naver.com>
2022-05-14 15:27:11 +0900

    week8/gsandoo

--- week8/gsandoo/video/youtube_index.js
+++ week8/gsandoo/video/youtube_index.js
@@ -74,4 +74,49 @@ function handleSubmit(e){
 }
 
 
+// video 값 최신화
+
+const $recommendedVideoList = document.querySelector('.recommendedVideoList');
+
+const API_KEY = "AIzaSyA7-LHliOao-kHPOc0Kp3f8_6hr8QY4H4U";
+ 
+function fetchVideo(){
+    fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%20%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=KR&key=${API_KEY}`
+        ).then(response => response.json())
+        .then(result=> result.items.map(video => videoSideTemplate(video)))
+        .catch(error => console.log(error))
+}
+fetchVideo();
+
+function videoSideTemplate(data){
+    const videoItem = `
+    <li class="videoItemContainer">
+        <a href=${"https://www.youtube.com/watch?v=${data.id}"}>
+            <div class="videoThumbnailContainer">
+            <img class="thumbnailImg" src=${data.snippet.thumbnails.high.url} />
+            </div>
+        </a>
+        <div class="videoDetailsContainer">
+            <a href=${"https://www.youtube.com/watch?v=${data.id}"}>
+                <div class="videoMetaDetails">
+                    <div class="videoTitle">
+                        <h3>${data.snippet.title}</h3>
+                    </div>
+                    <div class="videoMetaData">
+                        <p class="metaText">Chilli Music</p>
+                        <p class="extraMeta">
+                            <span class="metaText viewCount">${Number(data.statistics.viewCount) > 1000 ? (Number(data.statistics.viewCount)/1000).toFixed(0) + 'K views' :Number(data.statistics.viewCount)}</span>
+                            <span class="metaText">
+                                ${luxon.DateTime.fromISO(data.snippet.publishedAt).toRelative()}
+                            </span>        
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </li>
+    `
+    $recommendedVideoList.insertAdjacentHTML('beforeend',videoItem);
+}
 
